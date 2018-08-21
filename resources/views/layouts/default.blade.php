25cf96f5707a0a72cca99fe3590f9b5615b9740c
peer <peerforcher@foxmail.com>
2018-08-21 11:39:00 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,16 @@
 <html>
   <head>
     <title>@yield('title', 'Sample App') - Laravel 新手入门教程</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+        <div class="col-md-offset-1">
+            @yield('content')
+            @include('layouts._footer')
+        </div>
+    </div>
   </body>
 </html>
