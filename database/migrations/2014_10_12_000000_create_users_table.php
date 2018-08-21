535515ad1c5ac6c79af1d5427531de4af7957953
peer <peerforcher@foxmail.com>
2018-08-21 14:16:20 +0000

    Move user model to models folder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id');   //create int 自增 id
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            //由 rememberToken 方法为用户创建一个 remember_token 字段，用于保存『记住我』的相关信息。
             $table->rememberToken();
+            //由 timestamps方法创建了一个created_at和一个updated_at字段，分别用于保存用户的创建时间和更新时间。
             $table->timestamps();
         });
     }
