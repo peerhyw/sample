8bd9c8c42f270accbfaf0c8a4c3ac543c2e9a8e1
peer <peerforcher@foxmail.com>
2018-08-24 16:44:27 +0000

    debug

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -19,7 +19,7 @@ class StatusesController extends Controller
         ]);
 
         Auth::user()->statuses()->create([
-            'content' => $request['content'];
+            'content' => $request['content']
         ]);
 
         return redirect()->back();
